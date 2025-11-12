73bface30262a255d94367405e4fd8b70b904af5
Raghav Sharma <raghavsyt@gmail.com>
2025-11-13 04:40:39 +0530

    feat: added Post component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
+import { Post } from "./components/Feed/Post/Post";
+
 function App() {
   return (
     <div>
-      <h1>Hello, World</h1>
+      <Post />
     </div>
   );
 }
--- src/components/Feed/Post/Post.tsx
+++ src/components/Feed/Post/Post.tsx
@@ -1,3 +1,29 @@
 export const Post = () => {
-  return <h1>Feed post</h1>;
+  return (
+    <div className="w-full flex justify-center p-10">
+      <div className="w-full max-w-xl mx-auto">
+        <div>
+          <div className="flex items-start gap-4">
+            <img src="https://placehold.co/48" alt="avatar" />
+
+            <div>
+              <div>Theresa Webb</div>
+              <div>5 mins ago</div>
+            </div>
+          </div>
+
+          <div className="mt-6 flex gap-4">
+            <div>😊</div>
+
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
